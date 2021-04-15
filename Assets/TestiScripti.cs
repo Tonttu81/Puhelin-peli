eeab28c92f99f8e43c3dcf08dea73848db64df11
Tonttu81 <joni.pylkkonen@gmail.com>
2021-04-15 12:02:06 +0300

    Palikoiden valitseminen tehty

--- Assets/TestiScripti.cs
+++ Assets/TestiScripti.cs
@@ -24,7 +24,7 @@ public class TestiScripti : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
             Vector2 z = Camera.main.ScreenToWorldPoint(touch.position);
-            Vector3 touchPos = new Vector3(z.x, z.y, 0f);
+            touchPos = new Vector3(z.x, z.y, 0f);
 
 
             RaycastHit2D hit = Physics2D.Raycast(touchPos, new Vector2(0f, 0f), 1f);
@@ -32,8 +32,8 @@ public class TestiScripti : MonoBehaviour
             {
                 if (hit.collider.tag == "Circle")
                 {
-                    print("toimii");
                     grabbing = true;
+                    
                 }
             }
         }
@@ -46,7 +46,8 @@ public class TestiScripti : MonoBehaviour
         {
             Vector3 direction = (touchPos - transform.position).normalized;
 
-            transform.position += direction * throwSpeed * Time.deltaTime;
+            rb2D.velocity = direction * throwSpeed * Time.deltaTime;
         }
     }
+    
 }
