6d35e1b36cf2a9b2473bb6a97312ed16d2f2e2fc
Jesul1 <omajesse.vaarala@gmail.com>
2021-05-05 13:12:52 +0300

    Parempi MainMenu
    Uusi backgroundi
    uudet napit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject Cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cam.transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
+    }
+}
